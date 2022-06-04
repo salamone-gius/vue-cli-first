7b547b9c82c44f6d9f91b8a747a797c190438fc2
salamone-gius <salamone.gius@gmail.com>
2022-06-04 16:13:51 +0200

    'BaseTitle' component created

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,22 @@
+// qui va il mio HTML
+<template>
+
+  <h1>La mia prima app con Vue CLI!</h1>
+
+</template>
+
+// qui va il mio js
+<script>
+export default {
+    name: "BaseTitle"
+}
+</script>
+
+// qui va lo stile CSS
+<style>
+
+h1 {
+  color: darkgoldenrod;  
+}
+
+</style>
